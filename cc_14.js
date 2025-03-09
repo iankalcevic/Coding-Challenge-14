fee0eeb7eebd2769660fc89c4960e6af93c53d3e
iankalcevic <iankalcevic@usf.edu>
2025-03-09 14:34:46 -0400

    Task 3 - Highlighting High Priority Tickets

--- cc_14.js
+++ cc_14.js
@@ -31,3 +31,12 @@ function createSupportTicket(customerName, issueDescription, priority) { //Build
 createSupportTicket("Demar Derozan", "Payment declined", "Low");
 createSupportTicket("Jalen Smith", "Account hacked", "High");
 createSupportTicket("Scottie Pippen", "Password reset", "Medium");
+
+// Task 3 - Converting NodeLists to Array for Bulk Updates
+
+const highPriorityTickets = document.querySelectorAll(".ticket.high-priority");
+const highPriorityTicketsArray = Array.from(highPriorityTickets); //Create array for high priority tickets
+
+highPriorityTicketsArray.forEach(ticket => {
+    ticket.style.backgroundColor = "\e[42m" //Set green background color
+});
